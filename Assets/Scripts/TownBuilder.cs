d8dcc4ae8ddc0978ebe65b63b504c5a4fb1f76ae
BennettGN <bennettgodinhonelson@gmail.com>
2025-11-19 22:32:04 -0500

    Add Animation

--- Assets/Scripts/TownBuilder.cs
+++ Assets/Scripts/TownBuilder.cs
@@ -9,6 +9,8 @@ public class TownBuilder : MonoBehaviour
     //Contains all possible tiles
     [SerializeField] private List<Tile> allTiles;
     [SerializeField] private float tileSizeOffset = 1.0f;
+    [SerializeField ] private bool animateCollapse = false;
+    [SerializeField] private int animateDelay = 0;
 
     //Grid of tiles 
     private Tile[,] TownGrid;
@@ -29,6 +31,10 @@ public class TownBuilder : MonoBehaviour
     {
         TownGrid = new Tile[Width, Height];
         TilesToCollapse = new List<Vector2Int>();
+        TilesToCollapse.Clear();
+
+        //Start from middle of grid
+        TilesToCollapse.Add(new Vector2Int(Width / 2, Height / 2));
 
         offsets = new List<Vector2Int>
         {
@@ -38,88 +44,93 @@ public class TownBuilder : MonoBehaviour
             new Vector2Int(-1, 0)   // Left
         };
 
-        CollapseWord();
+        if (!animateCollapse)
+        {
+            CollapseWord();
+        }
     }
+
+
 
 
     public void CollapseWord()
     {
-        TilesToCollapse.Clear();
-
-        //Start from middle of grid
-        TilesToCollapse.Add(new Vector2Int(Width / 2, Height / 2));
 
         while (TilesToCollapse.Count > 0)
         {
+            Collapse();
+        }
 
-            int x = TilesToCollapse[0].x;
-            int y = TilesToCollapse[0].y;
+    }
 
-            if (TownGrid[x, y] != null)
+    public void Collapse()
+    {
+        int x = TilesToCollapse[0].x;
+        int y = TilesToCollapse[0].y;
+
+        if (TownGrid[x, y] != null)
+        {
+            TilesToCollapse.RemoveAt(0);
+            return;
+        }
+
+        List<Tile> possibleTiles = new List<Tile>(allTiles);
+
+        for (int i = 0; i < offsets.Count; i++)
+        {
+            Vector2Int neighborPos = new Vector2Int(x + offsets[i].x, y + offsets[i].y);
+
+            if (IsInBounds(neighborPos))
             {
-                TilesToCollapse.RemoveAt(0);
-                continue;
-            }
+                Tile neighborConnection = TownGrid[neighborPos.x, neighborPos.y];
 
-            List<Tile> possibleTiles = new List<Tile>(allTiles);
-
-            for(int i = 0; i < offsets.Count; i++) { 
-                Vector2Int neighborPos = new Vector2Int(x + offsets[i].x, y + offsets[i].y);
-                
-                if (IsInBounds(neighborPos))
+                //Remove nodes from possible tiles based on the possible tiles of the neighbor's adjacent connection
+                if (neighborConnection != null)
                 {
-                    Tile neighborConnection = TownGrid[neighborPos.x, neighborPos.y];
-
-                    //Remove nodes from possible tiles based on the possible tiles of the neighbor's adjacent connection
-                    if (neighborConnection != null)
+                    switch (i)
                     {
-                        switch (i)
-                        {
-                            case (int) Direction.Up:
-                                Intersect(possibleTiles, neighborConnection.Bottom.connectedTiles);
-                                break;
-                            case (int)Direction.Right:
-                                Intersect(possibleTiles, neighborConnection.Left.connectedTiles);
-                                break;
-                            case (int)Direction.Down:
-                                Intersect(possibleTiles, neighborConnection.Top.connectedTiles);
-                                break;
-                            case (int)Direction.Left:
-                                Intersect(possibleTiles, neighborConnection.Right.connectedTiles);
-                                break;
-                            default:
-                                Debug.LogError("Invalid direction parsing");
-                                break;
-                        }
+                        case (int)Direction.Up:
+                            Intersect(possibleTiles, neighborConnection.Bottom.connectedTiles);
+                            break;
+                        case (int)Direction.Right:
+                            Intersect(possibleTiles, neighborConnection.Left.connectedTiles);
+                            break;
+                        case (int)Direction.Down:
+                            Intersect(possibleTiles, neighborConnection.Top.connectedTiles);
+                            break;
+                        case (int)Direction.Left:
+                            Intersect(possibleTiles, neighborConnection.Right.connectedTiles);
+                            break;
+                        default:
+                            Debug.LogError("Invalid direction parsing");
+                            break;
                     }
-                    else
+                }
+                else
+                {
+                    //Add neighbor to TilesToCollapse and assume all tiles are possible until collapsed
+                    if (!TilesToCollapse.Contains(neighborPos))
                     {
-                        //Add neighbor to TilesToCollapse and assume all tiles are possible until collapsed
-                        if (!TilesToCollapse.Contains(neighborPos))
-                        {
-                            TilesToCollapse.Add(neighborPos);
-                        }
+                        TilesToCollapse.Add(neighborPos);
                     }
                 }
             }
-
-            if(possibleTiles.Count > 0)
-            {
-                Tile tilePicked = possibleTiles[Random.Range(0, possibleTiles.Count)];
-                TownGrid[x, y] = tilePicked;
-            }
-            else
-            {
-                //Render Default Tile if no possible tiles
-                TownGrid[x, y] = defaultTile;
-                Debug.Log("No possible tiles found, placing default tile at: " + x + ", " + y);
-            }
-
-            GameObject tileObject = Instantiate(TownGrid[x, y].Prefab, new Vector3(x * tileSizeOffset, 0, y * tileSizeOffset), TownGrid[x, y].Prefab.transform.rotation);
-            TilesToCollapse.RemoveAt(0);
-
         }
 
+        if (possibleTiles.Count > 0)
+        {
+            Tile tilePicked = possibleTiles[Random.Range(0, possibleTiles.Count)];
+            TownGrid[x, y] = tilePicked;
+        }
+        else
+        {
+            //Render Default Tile if no possible tiles
+            TownGrid[x, y] = defaultTile;
+            Debug.Log("No possible tiles found, placing default tile at: " + x + ", " + y);
+        }
+
+        GameObject tileObject = Instantiate(TownGrid[x, y].Prefab, new Vector3(x * tileSizeOffset, 0, y * tileSizeOffset), TownGrid[x, y].Prefab.transform.rotation);
+        TilesToCollapse.RemoveAt(0);
     }
 
     //Updates current tiles to remove all current tiles that are not in allowed tiles (currentTiles = currentTiles intersect allowedTiles)
@@ -140,11 +151,13 @@ public class TownBuilder : MonoBehaviour
         return position.x >= 0 && position.x < Width && position.y >= 0 && position.y < Height;
     }
 
-        // Loop through TilesToCollapse, each time we come across uncollapsed neighbor
-
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
-        
+        if (animateCollapse && TilesToCollapse.Count > 0)
+        {
+            Collapse();
+            System.Threading.Thread.Sleep(animateDelay);
+        }
     }
 }
